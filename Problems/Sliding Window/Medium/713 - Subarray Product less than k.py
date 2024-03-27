f46018306cbb214dcddc04a85a82a13565784d3d
pranav-2398 <76903645+pranav-2398@users.noreply.github.com>
2024-03-27 11:03:54 +0530

    Sliding Window/Medium/713 - Subarray Product less than k

---
+++ Problems/Sliding Window/Medium/713 - Subarray Product less than k.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        # Handle edge cases where k is 0 or 1 (no subarrays possible)
+        if k <= 1:
+            return 0
+
+        total_count = 0
+        product = 1
+
+        # Use two pointers to maintain a sliding window
+        left = 0
+        for right, num in enumerate(nums):
+            product *= num  # Expand the window by including the element at the right pointer
+
+            # Shrink the window from the left while the product is greater than or equal to k
+            while product >= k:
+                product //= nums[left]  # Remove the element at the left pointer from the product
+                left += 1
+
+            # Update the total count by adding the number of valid subarrays with the current window size
+            total_count += right - left + 1  # right - left + 1 represents the current window size
+
+        return total_count
+    
+s = Solution()
+# nums = [10, 5, 2, 6]
+nums = [10,9,10,4,3,8,3,3,6,2,10,10,9,3]
+# k = 100
+k = 19
+print(s.numSubarrayProductLessThanK(nums, k))
+
+
