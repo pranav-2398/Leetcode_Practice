963951b0382ecfb7474ce0a47b13d442359d5fd4
pranav-2398 <76903645+pranav-2398@users.noreply.github.com>
2024-06-13 21:54:25 +0530

    Arrays & Hashing/Easy/2037 - Min no of Moves to seat everyone

---
+++ Problems/Arrays & Hashing/Easy/2037 - Min no of Moves to seat everyone.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        seats.sort()
+        students.sort()
+        res = 0
+        for i in range(len(seats)):
+            res += abs(seats[i] - students[i])
+
+        return res
