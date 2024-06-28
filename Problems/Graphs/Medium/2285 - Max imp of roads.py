9da5e78bbb618191b9b198cd173dde5aa98e6359
pranav-2398 <76903645+pranav-2398@users.noreply.github.com>
2024-06-28 11:00:22 +0530

    Graphs/Medium/2285 - Max imp of roads

---
+++ Problems/Graphs/Medium/2285 - Max imp of roads.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
+        degree = [0] * n
+
+        for edge in roads:
+            degree[edge[0]] += 1
+            degree[edge[1]] += 1
+
+        degree.sort()
+
+        value = 1
+        total_importance = 0
+        for d in degree:
+            total_importance += value * d
+            value += 1
+
+        return total_importance
