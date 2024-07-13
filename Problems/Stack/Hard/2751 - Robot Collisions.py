75587fade90b81702c9a50cf26e2a4703001949e
pranav-2398 <76903645+pranav-2398@users.noreply.github.com>
2024-07-13 18:00:02 +0530

    Stack/Hard/2751 - Robot Collisions

---
+++ Problems/Stack/Hard/2751 - Robot Collisions.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
+        index_map = {p: i for i, p in enumerate(positions)}
+        stack = []
+
+        for p in sorted(positions):
+            i = index_map[p]
+
+            if directions[i] == 'R':
+                stack.append(i)
+            else:
+                while stack and healths[i]:
+                    j = stack.pop()
+                    if healths[i] > healths[j]:
+                        healths[j] = 0
+                        healths[i] -= 1
+                    elif healths[i] < healths[j]:
+                        healths[j] -= 1
+                        healths[i] = 0
+                        stack.append(j)
+                    else:
+                        healths[i] = healths[j] = 0
+        
+        return [h for h in healths if h > 0]
