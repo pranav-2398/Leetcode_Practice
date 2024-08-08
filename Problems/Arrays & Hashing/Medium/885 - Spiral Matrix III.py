d6c90c18eafab6a1880d21db14ccf1c599d3aa94
pranav-2398 <76903645+pranav-2398@users.noreply.github.com>
2024-08-08 22:08:11 +0530

    Arrays & Hashing/Medium/885 - Spiral Matrix III

---
+++ Problems/Arrays & Hashing/Medium/885 - Spiral Matrix III.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+        directions = [[0, 1], [1, 0], [0, -1] ,[-1, 0]]
+
+        res = []
+        r, c = rStart, cStart
+        steps = 1
+        i = 0
+        while len(res) < (rows * cols):
+            for x in range(2):
+                dr, dc = directions[i]
+                for _ in range(steps):
+                    if (0 <= r < rows and 0 <= c < cols):
+                        res.append([r, c])
+                    r, c = r + dr, c + dc
+                i = (i + 1) % 4
+            steps += 1
+        return res
+
+rows = 5
+cols = 6
+rStart = 1
+cStart = 4
+s = Solution()
+print(s.spiralMatrixIII(rows, cols, rStart, cStart))
