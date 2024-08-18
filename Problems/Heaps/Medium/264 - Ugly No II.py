e094bf0885fc754879e64df68e4c142aa71c576c
pranav-2398 <76903645+pranav-2398@users.noreply.github.com>
2024-08-18 12:46:02 +0530

    Heaps/Medium/264 - Ugly No II

---
+++ Problems/Heaps/Medium/264 - Ugly No II.py
@@ -0,0 +1,16 @@
+import heapq
+
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        minheap = [1]
+        visit = set()
+        factors = [2, 3, 5]
+        for i in range(n):
+            num = heapq.heappop(minheap)
+            if i == n - 1:
+                return num
+            
+            for f in factors:
+                if num * f not in visit:
+                    visit.add(num * f)
+                    heapq.heappush(minheap, num * f)
